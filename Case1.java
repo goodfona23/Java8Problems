0a9b15a6c2108c593ef33c1b234cdd2336fe8d78
Dhananjay Prasad <dhananjay.prasad04011993@gmail.com>
2025-09-07 08:23:07 +0530

    Add files via upload
    commit

--- Case1.java
+++ Case1.java
@@ -1,27 +1,39 @@
-package com;
+package collectors;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Case1 {
+	private static List<City> prepareTemperature()
+	{
+	    List<City> cities = new ArrayList<>();
+	    cities.add(new City("New Delhi", 33.5));
+	    cities.add(new City("Mexico", 14));
+	    cities.add(new City("New York", 13));
+	    cities.add(new City("Dubai", 43));
+	    cities.add(new City("London", 15));
+	    cities.add(new City("Alaska", 1));
+	    cities.add(new City("Kolkata", 30));
+	    cities.add(new City("Sydney", 11));
+	    cities.add(new City("Mexico", 14));
+	    cities.add(new City("Dubai", 43));
+	    return cities;
+	}
 
 	public static void main(String[] args) {
-
-		CustomFunctionalIntertface fi = (a, b) -> (a + b);
-		System.out.println(fi.add(45, 50));
-
-		Runnable thread1 = () -> {
-			int count = 1;
-			for (int i = 0; i < 10; i++) {
-				System.out.println(count++);
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		};
-
-		Thread t1 = new Thread(thread1);
-		t1.start();
-
+		
+		
+		//Collectors.toSet  This method will return Set instance and it doesn't contain any duplicates.
+		prepareTemperature().stream().filter(e->e.getTemperature()>10).collect(Collectors.toSet());
+		System.out.println(prepareTemperature());
+		
+		System.out.println(prepareTemperature()
+                .stream()
+                .collect(Collectors.groupingBy(
+                            City::getName,
+                            Collectors.mapping(
+                                City::getTemperature,
+                                Collectors.toList()))));
+		
 	}
 }
